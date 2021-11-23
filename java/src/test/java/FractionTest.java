c27dc95f8046fc118e0738b974f36a1dee6438ef
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-11-23 16:31:51 +0100

    Update and rename Fraction.java to FractionTest.java

--- java/src/test/java/FractionTest.java
+++ java/src/test/java/FractionTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Fraction {
+public class FractionTest {
 
 	@Test
 	public void renameMe() {
