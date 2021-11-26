511c183ab7b3bc31dc78055e260b14281d2c323e
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-11-26 19:22:18 +0100

    refactor the magic numbers

--- javascript/src/parser.js
+++ javascript/src/parser.js
@@ -1,12 +1,16 @@
+const NUMERATOR = 1;
+const DENOMINATOR = 2;
+
 const parse = (input) => {
   const isValid = !!input.trim().length > 0;
   if (!isValid) return { valid: false };
 
   const regex = /\{([-\d]+)\/([-\d]+)\}/g;
+
   const groups = regex.exec(input);
   return {
     valid: true,
-    result: parseInt(groups[1]) / parseInt(groups[2]),
+    result: parseInt(groups[NUMERATOR]) / parseInt(groups[DENOMINATOR]),
   };
 };
 
