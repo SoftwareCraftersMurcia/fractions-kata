840450ab68ce2dce18f937208ffd8482e68d77b9
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-11-26 19:19:17 +0100

    negative fraction

--- javascript/src/parser.js
+++ javascript/src/parser.js
@@ -2,12 +2,11 @@ const parse = (input) => {
   const isValid = !!input.trim().length > 0;
   if (!isValid) return { valid: false };
 
-  const regex = /\{(\d)\/(\d)\}/g;
+  const regex = /\{([-\d]+)\/(\d)\}/g;
   const groups = regex.exec(input);
-  console.log(groups);
   return {
     valid: true,
-    result: groups[1] / groups[2],
+    result: parseInt(groups[1]) / parseInt(groups[2]),
   };
 };
 
